b23ee8bac888221fda714e2ceb297eef4cba5a3f
Nasserfq <nonq70007@gmail.com>
2024-03-25 08:54:53 +0300

    production.rb file updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,9 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV.fetch("RAILS_SERVE_STATIC_FILES") { true }
   #? Nasser: From alfhd
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
-
+  #!Nasser: Added for production to see how it goes.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
